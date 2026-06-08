260f59782bf366b4fa52c022b90cbdc7580ba474
TKG <norikoissatkg@gmail.com>
2026-06-08 20:15:10 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} でログインしました`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
