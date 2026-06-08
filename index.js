34b9efd70270f8366205813b9048857dd8edb615
TKG <norikoissatkg@gmail.com>
2026-06-08 21:16:25 +0900

    response

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 require("dotenv").config();
 
-const { Client, GatewayIntentBits } = require("discord.js");
+const {
+  Client,
+  GatewayIntentBits,
+  Events
+} = require("discord.js");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
 
-client.once("ready", () => {
-  console.log(`${client.user.tag} でログインしました`);
+client.once(Events.ClientReady, () => {
+  console.log("Bot起動");
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "todo") {
+    const sub = interaction.options.getSubcommand();
+
+    if (sub === "add") {
+      const title =
+        interaction.options.getString("title");
+
+      await interaction.reply(
+        `受け取りました: ${title}`
+      );
+    }
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
